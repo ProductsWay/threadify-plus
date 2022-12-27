1469dbbb2f979b7a80dfaab8f3999cf482f43ee1
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-28 01:53:17 +0800

    fix: deploy to  vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
+import vercel from "solid-start-vercel";
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      adapter: vercel({ edge: true }),
+    }),
+  ],
 });
