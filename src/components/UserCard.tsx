e7dc8aa37fdefe6105923aec944493e7ccd2f6a5
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-29 02:21:15 +0800

    feat: add twitter link to user card
    - Add a link to the user's twitter profile
    - Replace the user's profile picture with the original one

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -9,17 +9,20 @@ export function UserCard({
   username: string;
   picture: string;
 }) {
+  const orignalImage = picture.replace("_normal", "");
   return (
     <div class="w-96 card card-side bg-base-100">
-      <figure>
-        <img src={picture} alt={name} />
-      </figure>
+      <A href={`https://twitter.com/${username}`} target="_blank">
+        <figure>
+          <img src={orignalImage} alt={name} class="mask mask-hexagon" />
+        </figure>
+      </A>
       <div class="card-body">
         <h2 class="card-title">
           {name} (@{username})
         </h2>
         <div class="justify-center card-actions">
-          <A href={"https://twitter.com/" + username} target="_blank">
+          <A href={`https://twitter.com/${username}`} target="_blank">
             <button class="btn btn-secondary">Follow</button>
           </A>
         </div>
