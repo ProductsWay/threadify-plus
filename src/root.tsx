2b771c28cdb4f282a2d7558b0025d7a7a0d6a24a
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-30 01:01:34 +0800

    fix: add user-scalable=no to viewport meta tag
    This prevents the user from zooming in on the page, which is not supported by the app.

--- src/root.tsx
+++ src/root.tsx
@@ -27,7 +27,10 @@ export default function Root() {
       <Head>
         <Title>ThreadifyPlus Reader App</Title>
         <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no"
+        />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5337133458846513"
