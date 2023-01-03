ed4671816d557b049703169c82f462cc53625a5d
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-04 00:38:28 +0800

    chore: rename download button

--- src/routes/thread/[id].tsx
+++ src/routes/thread/[id].tsx
@@ -135,7 +135,7 @@ export default function ThreadPage() {
             d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
           ></path>
         </svg>
-        Print Document
+        Save as PDF
       </button>
     </Show>
   );
