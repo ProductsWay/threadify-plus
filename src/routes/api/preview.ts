c9b3661c1f33fbef6d239ed6540bf6b82433c368
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-04 02:20:24 +0800

    feat: add preview link api

---
+++ src/routes/api/preview.ts
@@ -0,0 +1,34 @@
+import { APIEvent, json } from "solid-start/api";
+import { getLinkPreview } from "link-preview-js";
+import logger from "~/logger";
+
+export async function GET(event: APIEvent) {
+  // parse url params from query string
+  // e.g. /preview?url=https://example.com
+  const previewUrl = new URL(event.request.url);
+  const url = previewUrl.searchParams.get("url");
+  logger.info(`Fetching preview for ${url}`);
+
+  if (!url || !url.startsWith("http")) {
+    return json({ error: "No URL provided" }, { status: 400 });
+  }
+
+  const data = await getLinkPreview(url, {
+    followRedirects: "follow",
+    handleRedirects: (baseURL: string, forwardedURL: string) => {
+      logger.info("redirect", baseURL, forwardedURL);
+      const urlObj = new URL(baseURL);
+      const forwardedURLObj = new URL(forwardedURL);
+      if (
+        forwardedURLObj.hostname === urlObj.hostname ||
+        forwardedURLObj.hostname === "www." + urlObj.hostname ||
+        "www." + forwardedURLObj.hostname === urlObj.hostname
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  });
+  return json({ data, url });
+}
