6dd1ca81c319995c808a2f2dd9c62dc3826bfcd2
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-07 09:59:26 +0800

    refactor: move download button to bottom

--- src/routes/thread/[id].tsx
+++ src/routes/thread/[id].tsx
@@ -126,30 +126,30 @@ export default function ThreadPage() {
           >
             {/** @ts-expect-error use sharing-btn web component */}
           </sharing-btn>
+          <button
+            class="btn btn-md btn-secondary"
+            type="button"
+            onClick={() => downloadPdfFile(threadId)}
+          >
+            <svg
+              width="20"
+              height="20"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="mr-2 w-4 h-4 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+              ></path>
+            </svg>
+            Save as PDF
+          </button>
         </div>
       </div>
-      <button
-        class="absolute top-0 right-0 mt-2 btn btn-sm btn-secondary"
-        type="button"
-        onClick={() => downloadPdfFile(threadId)}
-      >
-        <svg
-          width="20"
-          height="20"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="mr-2 w-4 h-4 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-          ></path>
-        </svg>
-        Save as PDF
-      </button>
     </>
   );
 }
