97d8b892b415f9c70057729b04733bebb3a1cf24
Huynh Duc Dung <dunghd.it@gmail.com>
2023-01-10 19:37:18 +0800

    refactor: remove download pdf

--- src/routes/thread/[id].tsx
+++ src/routes/thread/[id].tsx
@@ -16,18 +16,6 @@ export function routeData({ params }: RouteDataArgs) {
   });
 }
 
-const downloadPdfFile = async (id?: string) => {
-  const element = document.getElementById("thread");
-  const opt = {
-    filename: `${id}-${Date.now()}.pdf`,
-    pagebreak: { mode: ["avoid-all", "css", "legacy"] },
-    html2canvas: { scale: 2, useCORS: true },
-  };
-
-  // @ts-expect-error global html2pdf from CDN
-  return window.html2pdf().set(opt).from(element).save();
-};
-
 export default function ThreadPage() {
   const data = useRouteData<typeof routeData>();
 
@@ -47,12 +35,6 @@ export default function ThreadPage() {
 
   return (
     <>
-      <script
-        src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
-        integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg=="
-        crossorigin="anonymous"
-        referrerpolicy="no-referrer"
-      ></script>
       <div class="container flex flex-col items-center py-8 px-4 mx-auto w-full text-center text-gray-700 shadow-xl">
         <SiteTitle>
           Thread {threadId} by {name}
@@ -126,28 +108,6 @@ export default function ThreadPage() {
           >
             {/** @ts-expect-error use sharing-btn web component */}
           </sharing-btn>
-          <button
-            class="btn btn-md btn-secondary"
-            type="button"
-            onClick={() => downloadPdfFile(threadId)}
-          >
-            <svg
-              width="20"
-              height="20"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              class="mr-2 w-4 h-4 stroke-current"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-              ></path>
-            </svg>
-            Save as PDF
-          </button>
         </div>
       </div>
     </>
