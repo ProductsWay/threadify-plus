953aee91277bd09e999e292dcbe999f06d115149
Huynh Duc Dung <dunghd.it@gmail.com>
2023-03-08 08:29:54 +0800

    feat: add openrelay for tracking

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,3 +1,9 @@
 import { mount, StartClient } from "solid-start/entry-client";
+import Tracker from '@openreplay/tracker';
+
+const tracker = new Tracker({
+  projectKey: "WJ7A1A1fqvasarIvxSDc",  
+});
+tracker.start();
 
 mount(() => <StartClient />, document);
