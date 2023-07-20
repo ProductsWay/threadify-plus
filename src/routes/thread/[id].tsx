b36ea9142e8dff72695ff893b59f5040abdcc48d
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-20 21:43:49 +0800

    feat: redirect to threader app
    fix: redirect to threader app

--- src/routes/thread/[id].tsx
+++ src/routes/thread/[id].tsx
@@ -1,134 +1,19 @@
-import { For, Show } from "solid-js";
-import { A, RouteDataArgs, useRouteData } from "solid-start";
-import { createServerData$ } from "solid-start/server";
-import { getThreadById } from "~/api-client";
-import { SEO } from "~/components/SEO";
-import SiteTitle from "~/components/SiteTitle";
-import TopThreads from "~/components/TopThreads";
-import { TwitterCard } from "~/components/TwitterCard";
-import { UserCard } from "~/components/UserCard";
-
+import { onMount } from "solid-js";
+import { RouteDataArgs, useRouteData } from "solid-start";
 import logger from "~/logger";
 
 export function routeData({ params }: RouteDataArgs) {
   logger.info("loading route data", params.id);
-  return createServerData$(async ([, id]) => await getThreadById(id), {
-    key: () => ["threads", params.id],
-  });
+  return params;
 }
 
-export default function ThreadPage() {
-  const data = useRouteData<typeof routeData>();
+export default function Thread() {
+  const { id } = useRouteData<typeof routeData>();
 
-  const { thread, id = "" } = data.latest ?? {};
+  onMount(() => {
+    // client-side redirect
+    window.location.href = `https://threadreaderapp.com/thread/${id}.html`;
+  });
 
-  const ids = Array.from(data.latest?.ids ?? []).reverse();
-  const threadId = data.latest?.ids?.[data.latest?.ids?.length - 1];
-
-  const name = thread?.[id]?.includes?.users?.[0].name ?? "";
-  const username = thread?.[id]?.includes?.users?.[0].username ?? "";
-  const avatar = thread?.[id]?.includes?.users?.[0].profile_image_url ?? "";
-  const content =
-    thread?.[threadId ?? id]?.data?.text ?? `A good thread by ${name}`;
-  const iamges =
-    thread?.[threadId ?? id]?.includes?.media
-      ?.filter((item) => item.type === "photo")
-      ?.map((item) => item.url) ?? [];
-
-  return (
-    <>
-      <div class="container flex flex-col items-center py-8 px-4 mx-auto w-full text-center text-gray-700">
-        <SiteTitle>
-          Thread #{threadId} by {name}
-        </SiteTitle>
-        <SEO
-          title={`Thread #${threadId} by ${name}`}
-          description={content}
-          image={iamges?.[0] ?? avatar}
-          content={content}
-        />
-        <Show when={thread?.[id]} fallback={<p>Loading...</p>}>
-          <div class="text-sm breadcrumbs">
-            <ul>
-              <li>
-                <A href="/">Home</A>
-              </li>
-              <li>Thread</li>
-              <li>
-                <A
-                  href={`https://twitter.com/${username}/status/${threadId}`}
-                  target="_blank"
-                >
-                  {threadId}
-                </A>
-              </li>
-            </ul>
-          </div>
-
-          <div
-            id="thread"
-            class="container flex flex-col items-center py-8 px-4 mx-auto w-full text-center"
-          >
-            <UserCard name={name} username={username} picture={avatar} />
-
-            <For each={ids}>
-              {(currentId, index) => (
-                <>
-                  <TwitterCard
-                    position={`${index.call(index) + 1}/${ids.length}`}
-                    createdAt={
-                      currentId === id
-                        ? thread?.[currentId]?.data?.created_at
-                        : undefined
-                    }
-                    text={thread?.[currentId]?.data?.text}
-                    textUrls={
-                      thread?.[currentId]?.data?.entities?.urls?.map(
-                        (item) => ({
-                          original_url: item.url,
-                          url: item.expanded_url,
-                        })
-                      ) ?? []
-                    }
-                    images={
-                      thread?.[currentId]?.includes?.media
-                        ?.filter((item) => item.type === "photo")
-                        ?.map((item) => item.url) ?? []
-                    }
-                    videoId={
-                      thread?.[currentId]?.includes?.media?.filter(
-                        (item) =>
-                          item?.type === "video" ||
-                          item?.type === "animated_gif"
-                      )?.length ?? 0 > 0
-                        ? currentId
-                        : undefined
-                    }
-                  />
-                </>
-              )}
-            </For>
-          </div>
-        </Show>
-        <div class="mb-10">
-          <script
-            async
-            type="module"
-            src="/scripts/sharing-btn/sharing-btn.esm.js"
-          ></script>
-          {/** @ts-expect-error use sharing-btn web component */}
-          <sharing-btn
-            url={`https://threadify.productsway.com/thread/${id}`}
-            description={content}
-            to="all"
-          />
-        </div>
-
-        <div class="divider">Most view threads</div>
-        <div class="overflow-y-scroll py-2 px-4 my-4 mx-2 mb-16 w-full scrollbar-thumb-gray-500 scrollbar-track-gray-100">
-          <TopThreads limit={10} />
-        </div>
-      </div>
-    </>
-  );
+  return <>Loading...</>;
 }
